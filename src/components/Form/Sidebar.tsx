5b7240cf807a104d8ee96cd1147b81bd9c7c2518
vinicius.souza <vinicius.souza@smartsupport.com.br>
2022-01-29 21:45:00 -0300

    config dashboard

--- src/components/Form/Sidebar.tsx
+++
@@ -1,9 +0,0 @@
-import { Box } from "@chakra-ui/react";
-
-export function Sidebar (){
-    return (
-        <Box as="aside" w="64" mr="8">
-
-        </Box>
-    )
-}
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,6 +1,68 @@
-import { Flex } from "@chakra-ui/react";
-import { Sidebar } from "../components/Form/Sidebar";
+import { Box, Flex, SimpleGrid, Text, theme } from "@chakra-ui/react";
+import dynamic from "next/dynamic";
+import { Sidebar } from "../components/Sidebar";
 import { Header } from "../components/Header";
+
+
+const Chart = dynamic(() => import('react-apexcharts'), {
+    ssr: false
+})
+
+const options = {
+    chart: {
+        toolbar : {
+            show: false,
+        },
+        zoom: {
+            enabled: false,
+        },
+        foreColor: theme.colors.gray[500], 
+    },
+    grid: {
+        show: false,
+    },
+    dataLabels: {
+        enabled: false,
+    },
+    tooltip: {
+        enabled: false,
+    },
+    xaxis: {
+        type: 'datetime',
+        axisBorder: {
+            color: theme.colors.gray[600]
+        },
+        axisTicks : {
+            color: theme.colors.gray[600]
+        },
+        categories: [
+            '2022-01-29T00:00:00:000Z',
+            '2022-01-28T00:00:00:000Z',
+            '2022-01-27T00:00:00:000Z',
+            '2022-01-26T00:00:00:000Z',
+            '2022-01-22T00:00:00:000Z',
+            '2022-01-20T00:00:00:000Z',
+            '2022-01-19T00:00:00:000Z',
+        ],
+    },
+    fill: {
+        opacity: 0.3,
+        type: 'gradient',
+        gradient : {
+            shade: 'dark', 
+            opacityFrom: 0.7,
+            opacityTo: 0.3,
+        }
+
+    }
+
+};
+
+const series = [
+    {
+        name: 'series1', data: [31, 123, 10 ,28 ,51 ,18, 59]
+    }
+]
 
 export default function Dashboard() {
     return (
@@ -9,6 +71,28 @@ export default function Dashboard() {
 
             <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
                 <Sidebar />
+
+                <SimpleGrid flex="1" gap="4" minChildWidth="320px" align="flex-start">
+                    <Box
+                     p="8"
+                     bg="gray.800"
+                     borderRadius={8}    
+                     pb="4"                 
+                    >
+                        <Text fontSize="lg" mb="4">Inscritos da semana</Text>
+                        <Chart options={options} series={series} type="area" height={160}/>
+                    </Box>
+                    <Box
+                     p="8"
+                     bg="gray.800"
+                     borderRadius={8}                     
+                     pb="4"
+                    >
+                        <Text fontSize="lg" mb="4">Taxa de abertura</Text>
+                        <Chart options={options} series={series} type="area" height={160}/>
+                    </Box>
+
+                </SimpleGrid>
             </Flex>
         </Flex>
         
